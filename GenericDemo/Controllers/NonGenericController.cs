2f59afa56319c515db8b736fd28d489ffc0a74f3
Nicole <nuling.huang@ehsn.com.tw>
2023-11-20 16:39:13 +0800

    上傳檔案

---
+++ GenericDemo/Controllers/NonGenericController.cs
@@ -0,0 +1,26 @@
+﻿using GenericDemo.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NonGenericController : ControllerBase
+    {
+
+
+        [HttpPost]
+        public ActionResult Post([FromForm] GiftImportRequest request)
+        {
+            if (request.File != null)
+            {
+                string folder = "Uploads";
+                string fileName = Guid.NewGuid().ToString() + "_" + request.File.FileName;
+                string filePath = Path.Combine(folder, fileName);
+                request.File.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            return Ok("假裝完成");
+        }
+    }
+}
---
+++ GenericDemo/Dtos/GiftImportRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace GenericDemo.Dtos
+{
+    public class GiftImportRequest
+    {
+        public IFormFile File { get; set; }
+    }
+}
