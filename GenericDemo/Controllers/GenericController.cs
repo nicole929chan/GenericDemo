2efd2f04eb72d5cbd183e164214170c05dacf63d
Nicole <nuling.huang@ehsn.com.tw>
2023-11-23 16:46:11 +0800

    讀取檔案(泛型)

---
+++ GenericDemo/Controllers/GenericController.cs
@@ -0,0 +1,60 @@
+﻿using GenericDemo.Dtos;
+using GenericDemo.Services.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenericController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] GiftImportRequest request)
+        {
+            try
+            {
+                string response = string.Empty;
+
+                if (request.File != null)
+                {
+                    string folder = "Uploads";
+                    string fileName = request.File.FileName;
+                    string randomName = Path.GetRandomFileName();
+                    string newFileName = Path.ChangeExtension(randomName, Path.GetExtension(fileName));
+                    string filePath = Path.Combine(folder, newFileName);
+
+                    using (FileStream fs = new(filePath, FileMode.Create))
+                    {
+
+                        await request.File.OpenReadStream().CopyToAsync(fs);
+                        // 在同一個程序中, 若要接續讀出剛才的存檔檔案, 必須手動Flush以關閉串流
+                        await fs.FlushAsync();
+                    }
+
+                    string saveFolder = "Uploads\\done";
+                    if (!Directory.Exists(saveFolder))
+                    {
+                        Directory.CreateDirectory(saveFolder);
+                    }
+                    string saveFileName = Path.ChangeExtension(randomName, ".txt");
+                    string savePath = Path.Combine(saveFolder, saveFileName);
+
+                    TextFileProcessor processor = new TextFileProcessor();
+                    var output = await processor.LoadData<Gift>(filePath);
+
+                    response = "File uploaded successfully";
+                }
+                else
+                {
+                    return BadRequest("No file provided in the request");
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internet Server Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ GenericDemo/Services/Generic/TextFileProcessor.cs
@@ -0,0 +1,49 @@
+﻿namespace GenericDemo.Services.Generic
+{
+    public class TextFileProcessor
+    {
+        public async Task<List<T>> LoadData<T>(string filePath) where T : class, new()
+        {
+            List<T> output = new List<T>();
+            T entry = new T();
+            var cols = entry.GetType().GetProperties();  // 取得欄位名稱(Reflection)
+
+            var lines = await File.ReadAllLinesAsync(filePath);
+            var headers = lines[0].Split(",");
+            var data = lines.Skip(1);
+
+            if (data.Count() < 2)
+            {
+                throw new IndexOutOfRangeException("The file was either empty or missing.");
+            }
+
+            foreach (var line in data)
+            {
+                var vals = line.Split(",");
+                entry = new T();
+
+                for (var i = 0; i < headers.Length; i++)
+                {
+                    foreach (var col in cols)
+                    {
+                        if (col.Name == headers[i])
+                        {
+                            if (col.PropertyType == typeof(bool))
+                            {
+                                col.SetValue(entry, Convert.ChangeType(Convert.ToInt16(vals[i]), col.PropertyType));
+                            }
+                            else
+                            {
+                                col.SetValue(entry, Convert.ChangeType(vals[i], col.PropertyType));
+                            }
+                        }
+                    }
+                }
+                output.Add(entry);
+            }
+
+
+            return output;
+        }
+    }
+}
